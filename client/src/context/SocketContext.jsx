f68034259cb5c23aff02cf176d58479085186287
rithvik reddy <147538307+rithvikreddybasani@users.noreply.github.com>
2024-08-03 10:39:00 +0530

    Update SocketContext.jsx

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -6,7 +6,7 @@ import { createContext, useCallback, useEffect, useMemo } from "react"
 import { toast } from "react-hot-toast"
 import { io } from "socket.io-client"
 
-const BACKEND_URL = "https://draw-code.vercel.app/socket"
+const BACKEND_URL = "https://draw-code.vercel.app/socketio"
 
 const SocketContext = createContext()
 
