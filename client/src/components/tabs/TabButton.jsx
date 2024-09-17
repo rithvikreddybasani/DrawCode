574e2d26a735f7843fb83309c552b2e380321331
rithvik reddy <147538307+rithvikreddybasani@users.noreply.github.com>
2024-09-17 12:19:31 +0530

    Update TabButton.jsx

--- client/src/components/tabs/TabButton.jsx
+++ client/src/components/tabs/TabButton.jsx
@@ -1,25 +1,31 @@
-import useChatRoom from "@/hooks/useChatRoom"
-import useTab from "@/hooks/useTabs"
-import TABS from "@/utils/tabs"
-import PropTypes from "prop-types"
+import useChatRoom from "@/hooks/useChatRoom";
+import useTab from "@/hooks/useTabs";
+import TABS from "@/utils/tabs";
+import PropTypes from "prop-types";
 
-function TabButton({ tabName, icon }) {
+function TabButton({ tabName, icon, url }) {
     const { activeTab, setActiveTab, isSidebarOpen, setIsSidebarOpen } =
-        useTab()
-    const { isNewMessage } = useChatRoom()
+        useTab();
+    const { isNewMessage } = useChatRoom();
 
-    const handleTabClick = (tabName) => {
-        if (tabName === activeTab) {
-            setIsSidebarOpen(!isSidebarOpen)
+    const handleTabClick = () => {
+        if (url) {
+            // If a URL is provided, open it in a new tab
+            window.open(url, '_blank');
         } else {
-            setIsSidebarOpen(true)
-            setActiveTab(tabName)
+            // Otherwise, handle tab switching
+            if (tabName === activeTab) {
+                setIsSidebarOpen(!isSidebarOpen);
+            } else {
+                setIsSidebarOpen(true);
+                setActiveTab(tabName);
+            }
         }
-    }
+    };
 
     return (
         <button
-            onClick={() => handleTabClick(tabName)}
+            onClick={handleTabClick}
             className="relative flex items-center justify-center"
         >
             {icon}
@@ -28,12 +34,13 @@ function TabButton({ tabName, icon }) {
                 <div className="absolute right-0 top-0 h-3 w-3 rounded-full bg-primary"></div>
             )}
         </button>
-    )
+    );
 }
 
 TabButton.propTypes = {
     tabName: PropTypes.string.isRequired,
     icon: PropTypes.element.isRequired,
-}
+    url: PropTypes.string, // Add url prop
+};
 
-export default TabButton
+export default TabButton;
